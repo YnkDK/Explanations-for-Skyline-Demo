1e4145aed480fb4d315ab5fd58346c7a84c69f93
Martin Storgaard <20140000@post.au.dk>
2015-05-06 15:39:35 +0200

    Finding skyline and skynot query

--- backend/hotels.php
+++ backend/hotels.php
@@ -1,29 +1,30 @@
 <?php
 require_once("headers.php");
+$skyline = [];
+$notSkyline = [];
 
-$string = file_get_contents("hotels.json");
-$json_a = json_decode($string, true);
+// Get the hotels as a php-array
+$hotels = json_decode(file_get_contents("hotels.json"), true);
+$file = fopen('skyNot.csv', 'r');
+
+$i = 0;
+while (($line = fgetcsv($file)) !== FALSE) {
+    $hotels[$i]["sn-price"]    = floatval($line[0]);
+    $hotels[$i]["sn-beach"]    = floatval($line[1]);
+    $hotels[$i]["sn-downtown"] = floatval($line[2]);
+    $hotels[$i]["sn-pools"]    = intval($line[3]);
+    $hotels[$i]["sn-rating"]   = floatval($line[4]);
+    $hotels[$i]["sn-stars"]    = intval($line[5]);
+
+    if(intval($line[0])+intval($line[1])+intval($line[2])+intval($line[3])+intval($line[4])+intval($line[5]) === 0) {
+        array_push($skyline, $hotels[$i]);
+    } else {
+        array_push($notSkyline, $hotels[$i]);
+    }
+    $i++;
+}
+
 echo json_encode([
-    "skyline" => [
-        [
-            "id" => 1,
-            "price" => 200,
-            "beach" => 0.1,
-            "downtown" => 0.8,
-            "pools" => 3,
-            "rating" => 4.7,
-            "stars" => 5
-        ],
-        [
-            "id" => 2,
-            "price" => 150,
-            "beach" => 0.1,
-            "downtown" => 1.1,
-            "pools" => 5,
-            "rating" => 4.9,
-            "stars" => 4
-        ]
-    ],
-    "notSkyline" => $json_a,
-    'GET' => $_GET["price"] === "true" ? true : false
+    "skyline" => $skyline,
+    "notSkyline" => $notSkyline
 ]);
