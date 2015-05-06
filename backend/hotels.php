483a6bca64dd23229bd7034d81b10042825f00ee
Martin Storgaard <20140000@post.au.dk>
2015-05-06 15:46:28 +0200

    Changed to lowerCamelCase

--- backend/hotels.php
+++ backend/hotels.php
@@ -9,12 +9,12 @@ $file = fopen('skyNot.csv', 'r');
 
 $i = 0;
 while (($line = fgetcsv($file)) !== FALSE) {
-    $hotels[$i]["sn-price"]    = floatval($line[0]);
-    $hotels[$i]["sn-beach"]    = floatval($line[1]);
-    $hotels[$i]["sn-downtown"] = floatval($line[2]);
-    $hotels[$i]["sn-pools"]    = intval($line[3]);
-    $hotels[$i]["sn-rating"]   = floatval($line[4]);
-    $hotels[$i]["sn-stars"]    = intval($line[5]);
+    $hotels[$i]["snPrice"]    = floatval($line[0]);
+    $hotels[$i]["snBeach"]    = floatval($line[1]);
+    $hotels[$i]["snDowntown"] = floatval($line[2]);
+    $hotels[$i]["snPools"]    = intval($line[3]);
+    $hotels[$i]["snRating"]   = floatval($line[4]);
+    $hotels[$i]["snStars"]    = intval($line[5]);
 
     if(intval($line[0])+intval($line[1])+intval($line[2])+intval($line[3])+intval($line[4])+intval($line[5]) === 0) {
         array_push($skyline, $hotels[$i]);
