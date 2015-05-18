d32b631fb1de23193176b1f9b6678bcc5c1c77be
Martin Storgaard <20140000@post.au.dk>
2015-05-18 13:47:17 +0200

    Removed unused variable

--- backend/ranges.php
+++ backend/ranges.php
@@ -5,27 +5,25 @@ require_once("Data.php");
 $db = new Data();
 $ex = $db->getExtremes();
 
-$range = array(
-    "price" => TRUE,
-    "priceFrom" => $ex["price"][0],
-    "priceTo" => $ex["price"][1],
-    "beach" => TRUE,
-    "beachFrom" => $ex["beach"][0],
-    "beachTo" => $ex["beach"][1],
-    "downtown" => TRUE,
-    "downtownFrom" => $ex["downtown"][0],
-    "downtownTo" => $ex["downtown"][1],
-    "pools" => TRUE,
-    "poolsFrom" => $ex["pools"][0],
-    "poolsTo" => $ex["pools"][1],
-    "rating" => TRUE,
-    "ratingFrom" => $ex["rating"][0],
-    "ratingTo" => $ex["rating"][1],
-    "stars" => TRUE,
-    "starsFrom" => $ex["stars"][0],
-    "starsTo" => $ex["stars"][1]
-);
-
 echo json_encode(array(
-    "data" => $range
+    "data" => array(
+        "price" => TRUE,
+        "priceFrom" => $ex["price"][0],
+        "priceTo" => $ex["price"][1],
+        "beach" => TRUE,
+        "beachFrom" => $ex["beach"][0],
+        "beachTo" => $ex["beach"][1],
+        "downtown" => TRUE,
+        "downtownFrom" => $ex["downtown"][0],
+        "downtownTo" => $ex["downtown"][1],
+        "pools" => TRUE,
+        "poolsFrom" => $ex["pools"][0],
+        "poolsTo" => $ex["pools"][1],
+        "rating" => TRUE,
+        "ratingFrom" => $ex["rating"][0],
+        "ratingTo" => $ex["rating"][1],
+        "stars" => TRUE,
+        "starsFrom" => $ex["stars"][0],
+        "starsTo" => $ex["stars"][1]
+    )
 ));
