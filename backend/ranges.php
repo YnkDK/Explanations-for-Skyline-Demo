da7e9d799442339e65d64c381d4d0d883b997126
Martin Storgaard <20140000@post.au.dk>
2015-05-18 13:44:07 +0200

    Using Data to get extreme points

--- backend/ranges.php
+++ backend/ranges.php
@@ -1,73 +1,30 @@
 <?php
 require_once("headers.php");
+require_once("Data.php");
 
-$hotels = json_decode(file_get_contents("hotels.json"), true);
+$db = new Data();
+$ex = $db->getExtremes();
 
 $range = array(
     "price" => TRUE,
-    "priceFrom" => INF,
-    "priceTo" => -INF,
+    "priceFrom" => $ex["price"][0],
+    "priceTo" => $ex["price"][1],
     "beach" => TRUE,
-    "beachFrom" => INF,
-    "beachTo" => -INF,
+    "beachFrom" => $ex["beach"][0],
+    "beachTo" => $ex["beach"][1],
     "downtown" => TRUE,
-    "downtownFrom" => INF,
-    "downtownTo" => -INF,
+    "downtownFrom" => $ex["downtown"][0],
+    "downtownTo" => $ex["downtown"][1],
     "pools" => TRUE,
-    "poolsFrom" => INF,
-    "poolsTo" => -INF,
+    "poolsFrom" => $ex["pools"][0],
+    "poolsTo" => $ex["pools"][1],
     "rating" => TRUE,
-    "ratingFrom" => INF,
-    "ratingTo" => -INF,
+    "ratingFrom" => $ex["rating"][0],
+    "ratingTo" => $ex["rating"][1],
     "stars" => TRUE,
-    "starsFrom" => INF,
-    "starsTo" => -INF
+    "starsFrom" => $ex["stars"][0],
+    "starsTo" => $ex["stars"][1]
 );
-
-for($i = 0; $i < count($hotels); $i++) {
-// Set ranges, i.e. min/max for each attribute
-    if($hotels[$i]["price"] < $range["priceFrom"]) {
-        $range["priceFrom"] = $hotels[$i]["price"];
-    }
-    if($hotels[$i]["price"] > $range["priceTo"]) {
-        $range["priceTo"] = $hotels[$i]["price"];
-    }
-
-    if($hotels[$i]["beach"] < $range["beachFrom"]) {
-        $range["beachFrom"] = $hotels[$i]["beach"];
-    }
-    if($hotels[$i]["beach"] > $range["beachTo"]) {
-        $range["beachTo"] = $hotels[$i]["beach"];
-    }
-
-    if($hotels[$i]["downtown"] < $range["downtownFrom"]) {
-        $range["downtownFrom"] = $hotels[$i]["downtown"];
-    }
-    if($hotels[$i]["downtown"] > $range["downtownTo"]) {
-        $range["downtownTo"] = $hotels[$i]["downtown"];
-    }
-
-    if($hotels[$i]["pools"] < $range["poolsFrom"]) {
-        $range["poolsFrom"] = $hotels[$i]["pools"];
-    }
-    if($hotels[$i]["pools"] > $range["poolsTo"]) {
-        $range["poolsTo"] = $hotels[$i]["pools"];
-    }
-
-    if($hotels[$i]["rating"] < $range["ratingFrom"]) {
-        $range["ratingFrom"] = $hotels[$i]["rating"];
-    }
-    if($hotels[$i]["rating"] > $range["ratingTo"]) {
-        $range["ratingTo"] = $hotels[$i]["rating"];
-    }
-
-    if($hotels[$i]["stars"] < $range["starsFrom"]) {
-        $range["starsFrom"] = $hotels[$i]["stars"];
-    }
-    if($hotels[$i]["stars"] > $range["starsTo"]) {
-        $range["starsTo"] = $hotels[$i]["stars"];
-    }
-}
 
 echo json_encode(array(
     "data" => $range
