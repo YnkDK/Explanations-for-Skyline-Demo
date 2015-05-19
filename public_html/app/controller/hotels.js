bde5accc0e21400d722ec8c0b8a7b3be5f3304c2
Martin Storgaard <20140000@post.au.dk>
2015-05-19 16:43:38 +0200

    Only showing previous values if current was changed

--- public_html/app/controller/hotels.js
+++ public_html/app/controller/hotels.js
@@ -9,10 +9,11 @@ controllers.HotelsController = function ($scope, $resource, $location, $filter) 
     $scope.inSkyline = [];
     $scope.notSkyline = [];
     $scope.filteredNotSkyline = [];
-    $scope.currentPage = 0;
+    $scope.currentPage = 1;
     $scope.numPerPage = 5;
     $scope.order = 'beach';
     $scope.edit = {};
+    $scope.currentSkyNot = {};
 
     // Load default settings for the query
     $resource(urlDefaultData).get(
@@ -42,7 +43,6 @@ controllers.HotelsController = function ($scope, $resource, $location, $filter) 
      * with the result
      */
     $scope.query = function () {
-        console.log($scope.data);
         $resource(urlHotelData).get(
             $scope.data
             , function (values) {
@@ -69,11 +69,16 @@ controllers.HotelsController = function ($scope, $resource, $location, $filter) 
     };
 
     $scope.changePage = function(newPage) {
+        console.log(newPage);
+        $scope.currentPage = newPage;
+
         if($scope.notSkyline.length > 0) {
             var begin = ((newPage - 1) * $scope.numPerPage)
                 , end = begin + $scope.numPerPage;
 
             $scope.filteredNotSkyline = $scope.notSkyline.slice(begin, end);
+
+            //$scope.currentPage = newPage;
         }
     };
 
@@ -91,40 +96,65 @@ controllers.HotelsController = function ($scope, $resource, $location, $filter) 
         $scope.status.isSecondOpen = false;
 
         //Current data
-        $scope.currentSkyNot = {
-            priceFrom : (hotel.snPrice === 0) ? '' : $scope.data.priceFrom,
-            beachFrom : (hotel.snBeach === 0) ? '' : $scope.data.beachFrom,
-            downtownFrom : (hotel.snDowntown === 0) ? '' :  $scope.data.downtownFrom,
-            poolsTo : (hotel.snPools === 0) ? '' : $scope.data.poolsTo,
-            ratingsTo : (hotel.snRating === 0) ? '' : $scope.data.ratingTo,
-            starsTo : (hotel.snStars === 0) ? '' : $scope.data.starsTo
-        };
+        resetPreviousDataValues();
 
-        $scope.data.priceFrom = (hotel.snPrice === 0) ? $scope.data.priceFrom : hotel.snPrice;
-        $scope.data.priceTo = ($scope.data.priceTo < $scope.data.priceFrom ? $scope.data.priceFrom : $scope.data.priceTo);
-
-        $scope.data.beachFrom = (hotel.snBeach === 0) ? $scope.data.beachFrom : hotel.snBeach;
-        $scope.data.beachTo = ($scope.data.beachTo < $scope.data.beachFrom ? $scope.data.beachFrom : $scope.data.beachTo);
-
-        $scope.data.downtownFrom = (hotel.snDowntown === 0) ? $scope.data.downtownFrom : hotel.snDowntown;
-        $scope.data.downtownTo = ($scope.data.downtownTo < $scope.data.downtownFrom ? $scope.data.downtownFrom : $scope.data.downtownTo);
-
-        $scope.data.poolsTo = (hotel.snPools === 0) ? $scope.data.poolsTo : hotel.snPools;
-        $scope.data.poolsFrom = ($scope.data.poolsFrom > $scope.data.poolsTo ? $scope.data.poolsTo : $scope.data.poolsFrom);
-
-        $scope.data.ratingTo = (hotel.snRating === 0) ? $scope.data.ratingTo : hotel.snRating;
-        $scope.data.ratingFrom = ($scope.data.ratingFrom > $scope.data.ratingTo ? $scope.data.ratingTo : $scope.data.ratingFrom);
-
-        $scope.data.starsTo = (hotel.snStars === 0) ? $scope.data.starsTo : hotel.snStars;
-        $scope.data.starsFrom = ($scope.data.starsFrom > $scope.data.starsTo ? $scope.data.starsTo : $scope.data.starsFrom);
+        if(hotel.snPrice && $scope.data.price) {
+            $scope.currentSkyNot.priceFrom = $scope.data.priceFrom;
+            $scope.data.priceFrom = parseFloat(hotel.snPrice);
+            $scope.data.priceTo = ($scope.data.priceTo < $scope.data.priceFrom ? $scope.data.priceFrom : $scope.data.priceTo);
+        }
+        if(hotel.snBeach && $scope.data.beach) {
+            $scope.currentSkyNot.beachFrom = $scope.data.beachFrom;
+            $scope.data.beachFrom = parseFloat(hotel.snBeach);
+            $scope.data.beachTo = ($scope.data.beachTo < $scope.data.beachFrom ? $scope.data.beachFrom : $scope.data.beachTo);
+        }
+        if(hotel.snDowntown && $scope.data.downtown) {
+            $scope.currentSkyNot.downtownFrom = $scope.data.downtownFrom;
+            $scope.data.downtownFrom = parseFloat(hotel.snDowntown);
+            $scope.data.downtownTo = ($scope.data.downtownTo < $scope.data.downtownFrom ? $scope.data.downtownFrom : $scope.data.downtownTo);
+        }
+        if(hotel.snPools && $scope.data.pools) {
+            $scope.currentSkyNot.poolsTo = $scope.data.poolsTo;
+            $scope.data.poolsTo = parseFloat(hotel.snPools);
+            $scope.data.poolsFrom = ($scope.data.poolsFrom > $scope.data.poolsTo ? $scope.data.poolsTo : $scope.data.poolsFrom);
+        }
+        if(hotel.snRating && $scope.data.rating) {
+            $scope.currentSkyNot.ratingTo = $scope.data.ratingTo;
+            $scope.data.ratingTo = parseFloat(hotel.snRating);
+            $scope.data.ratingFrom = ($scope.data.ratingFrom > $scope.data.ratingTo ? $scope.data.ratingTo : $scope.data.ratingFrom);
+        }
+        if(hotel.snStars && $scope.data.stars) {
+            $scope.currentSkyNot.starsTo = $scope.data.starsTo;
+            $scope.data.starsTo = parseFloat(hotel.snStars);
+            $scope.data.starsFrom = ($scope.data.starsFrom > $scope.data.starsTo ? $scope.data.starsTo : $scope.data.starsFrom);
+        }
     };
 
     function resetPreviousDataValues(){
-        $scope.currentSkyNot.priceFrom = "";
-        $scope.currentSkyNot.beachFrom = "";
-        $scope.currentSkyNot.downtownFrom = "";
-        $scope.currentSkyNot.poolsTo = "";
-        $scope.currentSkyNot.ratingsTo = "";
-        $scope.currentSkyNot.starsTo = "";
+        $scope.currentSkyNot = {};
+    }
+
+    $scope.findHotel = function(hotel) {
+        if(hotel.score === 0.0) {
+            // In skyline
+        } else {
+            var found = false;
+            // Open the correct accordion
+            $scope.status.isFirstOpen = false;
+            $scope.status.isSecondOpen = true;
+            for(var i = 0; i < $scope.notSkyline.length; i++) {
+                $scope.changePage(i);
+                for(var j = 0; j < $scope.filteredNotSkyline.length; j++) {
+                    if($scope.filteredNotSkyline[j] === hotel) {
+                        found = true;
+                        $scope.currentPage = i;
+                        break;
+                    }
+                }
+                if(found) break;
+            }
+            hotel.highlight = true;
+            // TODO: Scroll to hotel
+        }
     }
 };
