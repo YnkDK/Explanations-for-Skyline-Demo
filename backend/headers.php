41d4a7d46c100ca3199d537728d47a85c3cb1c78
Martin Storgaard <20140000@post.au.dk>
2015-05-25 20:10:07 +0200

    session_start()

--- backend/headers.php
+++ backend/headers.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type, Pragma, Connection, Cache-Control");
 header("Access-Control-Allow-Methods: HEAD, GET, POST, OPTIONS, DELETE, PUT");
